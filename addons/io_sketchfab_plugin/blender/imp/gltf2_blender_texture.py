39f5bb603f016a04c151b75e98cc54f2dda1afe5
Loïc Norgeot <loic.norgeot@sketchfab.com>
2021-10-20 12:42:05 +0200

    Fixes bad module imports after latest GlTF import update

--- addons/io_sketchfab_plugin/blender/imp/gltf2_blender_texture.py
+++ addons/io_sketchfab_plugin/blender/imp/gltf2_blender_texture.py
@@ -16,8 +16,8 @@ import bpy
 
 from .gltf2_blender_image import BlenderImage
 from ..com.gltf2_blender_conversion import texture_transform_gltf_to_blender
-from io_scene_gltf2.io.com.gltf2_io import Sampler
-from io_scene_gltf2.io.com.gltf2_io_constants import TextureFilter, TextureWrap
+from ...io.com.gltf2_io import Sampler
+from ...io.com.gltf2_io_constants import TextureFilter, TextureWrap
 
 def texture(
     mh,
